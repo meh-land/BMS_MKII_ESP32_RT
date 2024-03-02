4fc39dbc589c30dda6fa6f6a073478e393a45c78
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2024-03-02 08:34:24 +0200

    forgot to save changes to main.ino XDD

--- main.ino
+++ main.ino
@@ -8,53 +8,26 @@
 // FreeRTOS is prebuilt in ESP32 Library (Yay!)
 
 
-/**Defining Pins*/ // #TODO: Change #define to enums
-// ADC Pins
-
-
-
-
-
-
-
-
-// Balancing Pins
-
-
-// Contactor Pins
-
-
-
-/**Temp*/
-
-
-
-/**ACS712*/
-
-
-/**Cell Voltage Level*/
-
-
-
 
 
 void setup() {
+	
   // Serial.begin(9600);
   
   // Temp Setup
-  
+  tempSensor_init();
 
   // ACS712 Setup
-  
+  currentSensor_init();
 
   // Cell Voltage Setup
-  
+  cellVoltageReading_init();
   
   // Balancing Pins Setup
-  
+  balancing_init();
   
   // Contactor Pins Setup
-  
+  contactor_init();
 
   xTaskCreate(void_RTOSTask_1_ReadCellVoltageLevel, "Read Cell Voltage Level", 1024, NULL, READ_VOLTAGE_PRIORITY, NULL);
   xTaskCreate(void_RTOSTask_2_ReadTempLevel, "Read Temperature Level", 1024, NULL, READ_TEMP_PRIORITY, NULL);
